4fe2bfe88913bc641e89714173bbfee011677d33
meguoe <meguoe@163.com>
2023-07-21 19:02:32 +0800

    refactor: Refactoring the compressedForm function

--- src/ipv6/compressedForm.ts
+++ src/ipv6/compressedForm.ts
@@ -1,4 +1,4 @@
-import { isValidIP } from './index';
+import { ip2long, expandedForm, isValidIP } from './index';
 
 /**
  * Compresses an expanded IPv6 address into shortened form.
@@ -14,11 +14,19 @@ import { isValidIP } from './index';
 
 export function compressedForm(ip: string): string | false {
   if (!isValidIP(ip)) return false;
+  if (ip2long(ip) === 0n) return '::';
+  
+  ip = expandedForm(ip) as string;
   const sections: string[] = ip.split(':');
-  
-  return sections.map((section: string) => {
+  const compress = sections.map((section: string) => {
     const _section: number = parseInt(section, 16);
-    return _section ? _section.toString(16) : '';
-  }).join(':').replace(/:{2,}/g, '::');
+    return _section ? _section.toString(16) : 'X';
+  }).join(':');
+
+  const regExp = [ /(X:X:X:X:X:X:X)/, /(X:X:X:X:X:X)/, /(X:X:X:X:X)/, /(X:X:X:X)/, /(X:X:X)/, /(X:X)/ ];
+  for (let i = 0; i < regExp.length; i++) {
+    if (compress.match(regExp[i])) return compress.replace(regExp[i], ':').replace(':::', '::').replaceAll('X', '0');
+  }
+
+  return compress.replaceAll('X', '0');
 }
-  
